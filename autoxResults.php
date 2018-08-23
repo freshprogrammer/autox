40526d421b043555b7aefc0272977bc8168bd625
Douglas Crafts <Doug@FreshProgramming.com>
2018-08-23 03:37:26 -0500

    Added basic result JS with row highlight

--- autoxResults.php
+++ autoxResults.php
@@ -1,9 +1,92 @@
+<?php
+	function GetInput($name, $checkPost=true, $checkGet=true)
+	{
+		if($checkPost && isset($_POST[$name]))
+		{
+			$input = $_POST[$name];
+		}
+		else if($checkGet && isset($_GET[$name]))
+		{
+			$input = $_GET[$name];
+		}
+		else
+		{
+			$input = "";
+		}
+		return trim($input);
+	}
+	
+	$raceNumber = GetInput("racenumber");
+?>
 <link rel="stylesheet" href="default.css">
+<script>
+
+function formatPage()
+{
+	var numberOffset = -2;//start of row is 2 cell to the left
+	/*
+	::assumes this format::
+	header row with colspan in first cell and last cells are 'Total' & 'Diff.'
+	data row
+	pos/number/name/car/color/runX (1-10)/Total/Diff
+	
+	*/
+	
+	
+	console.log("scanning");
+	var raceNumber = '<?php echo $raceNumber ?>';
+	if(!(!isNaN(parseFloat(raceNumber)) && isFinite(raceNumber)))
+	{
+		//not a number
+		raceNumber = "zzz";
+	}
+	console.log("race number = '"+raceNumber+"'");
+	
+	var firstDataCell = 0;
+	var cols = 0;
+	var highlightedRowStart = -1;
+	
+	var tds = document.getElementsByTagName("td");
+	//find start of data
+	for (i = 0; i < tds.length; i++) 
+	{ 
+		if(tds[i].textContent == "Total")
+		{
+			firstDataCell = i+2;
+			break;
+		}
+	}
+	for (i = firstDataCell; i < tds.length; i++) 
+	{ 
+		if(tds[i].textContent == raceNumber)
+		{
+			console.log("found it - "+i);
+			cols = tds[i].parentElement.children.length;
+			highlightedRowStart = i+numberOffset;
+		}
+	}
+	
+	//highlight row
+	if(highlightedRowStart!=-1)
+	{
+		tds[highlightedRowStart].parentElement.className = "highlightedRacer";
+		for (i = highlightedRowStart; i < highlightedRowStart+cols; i++) 
+		{ 
+			//process row
+		}
+	}	
+}
+</script>
+
 <?php
 
-$dataURL = "http://www.ntaxs.com/uploads/2/5/5/1/25513290/2018_4_july_21_fin.htm";
+//$dataURL = "http://live.ntaxs.com/";
+$dataURL = "http://autox.lscbmwcca.org/";
 
 $dataFile = file_get_contents($dataURL, true);
 
 echo $dataFile;
 ?>
+<script>
+formatPage();
+</script>
--- index.php
+++ index.php
@@ -1,26 +1,30 @@
 <head>
+<meta name='viewport' content='width=device-width, initial-scale=1.0'>
 <title>DFW Autocross stuff</title>
 <link rel="stylesheet" href="default.css">
 </head>
 <body>
 
-<font size=5><b>DFW Autocross stuff</b></font><br>
-<br>
-<a href='autoxResults.php'>Processed Data</a><br>
+<table>
+<tbody align='center'>
 
-<br>
-<br>
+<tr><td><h2>DFW Autocross stuff</h2></td></tr>
 
-<a href='http://autox.lscbmwcca.org/'>Lone Star Chapter BMW CCA - Live - http://autox.lscbmwcca.org</a><br>
-<a href='http://live.ntaxs.com/'>North Texas Autocross Series - Live - http://live.ntaxs.com</a><br>
-<br>
-<a href='http://www.dlbracing.com/'>DLB Racing</a><br>
-<br>
-<a href='https://www.lightspeedimages.com/'>Light Speed Images</a><br>
+<tr><td><a href='autoxResults.php'>Processed Data</a></td></tr>
+<tr><td></td></tr>
+<tr><td>Lone Star Chapter BMW CCA (<a href='http://autox.lscbmwcca.org/'>live data</a>) - <a href='http://autox.lscbmwcca.org/'>http://autox.lscbmwcca.org</a></td></tr>
+<tr><td>North Texas Autocross Series (<a href='http://live.ntaxs.com/'>live data</a>) - <a href='http://live.ntaxs.com/'>http://live.ntaxs.com</a></td></tr>
+<tr><td></td></tr>
+<tr><td><a href='http://www.dlbracing.com/' target='_blank'>DLB Racing</a></td></tr>
+<tr><td></td></tr>
+<tr><td><a href='https://www.lightspeedimages.com/' target='_blank'>Light Speed Images</a></td></tr>
+
+</tbody>
+</table>
 
 </body>
 <footer>
-Made by Doug <a href=https://github.com/freshprogrammer>GitHub</a>
+Made by Doug - Source on <a href='https://github.com/freshprogrammer/autox' target='_blank'>GitHub</a>
 </footer>
 
 <?php
