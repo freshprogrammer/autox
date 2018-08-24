40e4fb3769a6a2b5e8748c6db59ab33d0260d169
Douglas Crafts <Doug@FreshProgramming.com>
2018-08-24 02:27:35 -0500

    Added best-worst & first-last

--- autoxResults.php
+++ autoxResults.php
@@ -34,7 +34,7 @@ class racerRec{
 		this.best = 0;
 	}
 	
-	calcBest()
+	calcBest()//return best time or dnf if no non dnf times found
 	{
 		this.best = 'dnf';
 		for (var i = 0; i < this.runs.length; i++) 
@@ -58,6 +58,50 @@ class racerRec{
 			return 999999;
 		else return this.best;
 	}
+	
+	GetRawTime(input)
+	{
+		let thisTime = '999999';
+		if(typeof input === 'undefined' || input === null || input=="")
+			return thisTime;
+		if(input.indexOf("+")!=-1)
+			thisTime = input.substr(0,input.indexOf("+"));
+		else
+			thisTime = input;
+		
+		return thisTime;
+	}
+	
+	FirstToLast()
+	{
+		if(this.number==74)
+			var debug = true;
+		let lastX = this.runs.length;
+		while(--lastX>=0 && (this.GetRawTime(this.runs[lastX])<=0 || this.GetRawTime(this.runs[lastX])>=999999));
+		return this.GetRawTime(this.runs[0]) - this.GetRawTime(this.runs[lastX]);
+	}
+	
+	BestToWorst()
+	{
+		let worst = 0;
+		let best =999999;
+		for (var i = 0; i < this.runs.length; i++) 
+		{
+			let thisTime = this.GetRawTime(this.runs[i]);
+			if(thisTime<best && thisTime>1)
+				best = thisTime;
+			if(thisTime>worst && thisTime<999999)
+				worst = thisTime;
+		}
+		if(worst==0)
+			return 0;
+		return worst-best;
+	}
+}
+
+function roundTime(input)
+{
+	return Math.round(1000*input)/1000
 }
 
 function formatPage()
@@ -147,6 +191,8 @@ function formatPage()
 	bottomTable += "<th>Car</th>";
 	bottomTable += "<th>Best</th>";
 	bottomTable += "<th>Diff</th>";
+	bottomTable += "<th>First-Last</th>";
+	bottomTable += "<th>Best-Worst</th>";
 	bottomTable += "</tr>";
 	var lastTime = data[0].best;
 	for (i = 0; i < data.length; i++) 
@@ -162,7 +208,9 @@ function formatPage()
 		bottomTable += "<td>"+data[i].name+"</td>";
 		bottomTable += "<td>"+data[i].car+"</td>";
 		bottomTable += "<td>"+data[i].best+"</td>";
-		bottomTable += "<td>+"+Math.round(1000*(data[i].best-lastTime))/1000+"</td>";
+		bottomTable += "<td>+"+roundTime(data[i].best-lastTime)+"</td>";
+		bottomTable += "<td>"+roundTime(data[i].FirstToLast())+"</td>";
+		bottomTable += "<td>"+roundTime(data[i].BestToWorst())+"</td>";
 		bottomTable += "</tr>";
 	}
 	bottomTable += "</tbody>";
